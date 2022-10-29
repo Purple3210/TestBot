8576dc1471daf4f95e048b94cfe8cd749ec038bb
Purple3210 <106269914+Purple3210@users.noreply.github.com>
2022-10-29 19:37:56 +0200

    Changes V0.2

---
+++ pythonProject/cogs/tickets.py
@@ -0,0 +1,29 @@
+import discord.ui
+from discord.commands import slash_command
+from discord.ext import commands
+
+
+class tickets(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command()
+    async def embed(self, ctx):
+        modal = testmodal(title="Erzeuge ein Embed")
+        await ctx.send_modal(modal)
+
+
+def setup(bot):
+    bot.add_cog(tickets(bot))
+
+
+class testmodal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            discord.ui.InputText(
+                label="Embed Titel",
+                placeholder="Placeholder"
+            )
+            * args,
+            **kwargs
+        )
